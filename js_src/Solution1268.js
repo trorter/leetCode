250715759a962dcd5c3441f5691fe5019d93604a
andrey <auledovskih@gmail.com>
2022-06-19 10:09:42 +0300

    Solution1268 js

---
+++ js_src/Solution1268.js
@@ -0,0 +1,38 @@
+/**
+ * @param {String} firstWord
+ * @param {String} secondWord
+ */
+const findCommonPrefix = (firstWord, secondWord) => {
+    const minLength = Math.min(firstWord.length, secondWord.length)
+    for (let i = 0; i < minLength; i++) {
+        if (firstWord[i] !== secondWord[i]) {
+            return i
+        }
+    }
+    return minLength
+}
+
+/**
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+const suggestedProducts = function(products, searchWord) {
+    products.sort()
+
+    const result = Array.from(Array(searchWord.length), () => [])
+
+    for (product of products) {
+        const prefixLength = findCommonPrefix(product, searchWord)
+
+        for (let i = 0; i < prefixLength; i++) {
+            if (result[i].length < 3) {
+                result[i].push(product)
+            }
+        }
+    }
+
+    return result
+};
+
+console.log(suggestedProducts(["mobile","mouse","moneypot","monitor","mousepad"], "mouse"))
