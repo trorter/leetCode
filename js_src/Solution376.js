77471a6094cdf2a29807ae883ff3774239f0e3f4
andrey <auledovskih@gmail.com>
2022-07-03 09:36:24 +0300

    Solution376 js

---
+++ js_src/Solution376.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const wiggleMaxLength = (nums) => {
+  if (nums.length < 2) {
+    return nums.length
+  }
+
+  let up = 1
+  let down = 1
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      up = down + 1
+    } else if (nums[i] < nums[i - 1]) {
+      down = up + 1
+    }
+  }
+
+  return Math.max(up, down)
+}
+
+console.log(wiggleMaxLength([0,0]))
