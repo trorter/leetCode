8c069ca5e331a7fcc40214cb86df24183459bffa
Andrei Ledovskikh <auledovskih@gmail.com>
2022-07-08 18:10:01 +0400

    Solution347

---
+++ ts_src/Solution347.ts
@@ -0,0 +1,9 @@
+const topKFrequent = (nums: Array<number>, k: number): Array<number> => {
+    const map = new Map<number, number>()
+
+    for(let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1)
+    }
+
+    return [...map.entries()].sort((a, b) => b[1] - a[1]).slice(0, k).map(a => a[0])
+}
