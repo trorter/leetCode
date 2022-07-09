3cc4d343a34282e9a66d5b1a8a5d7cb3b9908842
Andrei Ledovskikh <auledovskih@gmail.com>
2022-07-09 22:01:31 +0400

    Solution1871

---
+++ ts_src/Solution1871.ts
@@ -0,0 +1,34 @@
+const canReach1 = (s: string, minJump: number, maxJump: number): boolean => {
+  if (s[s.length - 1] === '1') {
+    return false
+  }
+
+  if (s.split('0').some(item => maxJump <= item.length)) {
+    return false
+  }
+
+  const queue: Array<number> = []
+  const arr: Array<string> = s.split('')
+
+  queue.push(0)
+  arr[0] = '1'
+
+  while (queue.length !== 0) {
+    let next: number = queue.pop() || 0
+
+    if (next === s.length - 1) {
+      return true
+    }
+
+    const end = Math.min(s.length - 1, next + maxJump)
+    for (let i = next + minJump; i <= end; i++) {
+
+      if (arr[i] === '0') {
+        queue.push(i)
+        arr[i] = '1'
+      }
+    }
+  }
+
+  return false
+}
