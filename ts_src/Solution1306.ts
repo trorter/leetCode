269a2dcdeeb284159d24ea3e65c60eb3c7a358ac
Andrei Ledovskikh <auledovskih@gmail.com>
2022-07-09 20:22:33 +0400

    add tsconfig

---
+++ ts_src/Solution1306.ts
@@ -0,0 +1,21 @@
+const canReach = (arr: number[], start: number): boolean => {
+  let memo: Array<boolean> = Array.from(new Array(arr.length), () => false)
+
+  const dfs = (index: number): boolean => {
+    if (index < 0 || arr.length <= index) {
+      return false
+    }
+    if (arr[index] === 0) {
+      return true
+    }
+    if (memo[index]) {
+      return false
+    }
+
+    memo[index] = true
+
+    return dfs(index + arr[index]) || dfs(index - arr[index])
+  }
+
+  return dfs(start)
+}
