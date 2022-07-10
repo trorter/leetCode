99d293bcb2eb721f69c780d76666d95f250aa9b8
Andrei Ledovskikh <auledovskih@gmail.com>
2022-07-10 11:29:49 +0400

    Solution746

---
+++ ts_src/Solution746.ts
@@ -0,0 +1,12 @@
+const minCostClimbingStairs = (cost: number[]): number => {
+  const array: Array<number> = Array.from(Array(cost.length + 1), () => 0)
+
+  for (let i = 2; i < array.length; i++) {
+    const prePrev = array[i - 2] + cost[i -2]
+    const pre = array[i - 1] + cost[i -1]
+
+    array[i] = Math.min(prePrev, pre)
+  }
+
+  return array[array.length - 1]
+}
